32c21d7d6faec2f1e34ac784c797b517d1dc58b7
Faye Rogers <fr7@sanger.ac.uk>
2018-10-29 16:04:21 +0000

    New config for iris_tokens

--- config.py
+++ config.py
@@ -4,8 +4,16 @@ config={
         'user' : os.environ['MYSQL_USER'],
         'password' : os.environ['MYSQL_PASS'],
         'host' : os.environ['MYSQL_HOST'],
-        'database' : 'tmuris_isoseq',
+        'database' : 'test_isoseq',
         'port' : os.environ['MYSQL_PORT'],
         'raise_on_warnings': True
 }
 
+config_iris={
+        'user' : os.environ['MYSQL_USER'],
+        'password' : os.environ['MYSQL_PASS'],
+        'host' : os.environ['MYSQL_HOST'],
+        'database' : 'iris_test',
+        'port' : os.environ['MYSQL_PORT'],
+        'raise_on_warnings': True
+}
